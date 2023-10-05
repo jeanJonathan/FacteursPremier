3656b6a5d4cb984430c6eb278270a3625901377d
Jean Jonathan <jeanjonathank2@gmail.com>
2023-10-05 21:35:37 +0200

    initial commit

---
+++ src/main/java/facteursPremiers/Main.java
@@ -0,0 +1,10 @@
+package facteursPremiers;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
+
+
---
+++ src/test/java/facteursPremiers/FacteursPremiersTest.java
@@ -0,0 +1,7 @@
+package facteursPremiers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacteursPremiersTest {
+
+}
